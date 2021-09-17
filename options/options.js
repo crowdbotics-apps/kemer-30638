d65bb520da27141061a1a528004ae68196343c1a
Crowdbotics <team@crowdbotics.com>
2021-09-17 15:25:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "kemer_30638Identifier",
+  url: "https://kemer_30638Identifier.botics.co",
+  api: "https://kemer_30638Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
